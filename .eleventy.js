58ed3625866f288a158e42f13c1eda74aaf97c35
Scott Herrington <scott.herrington@me.com>
2023-10-27 15:57:35 +0100

    template complete

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
-
 // Filters
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
@@ -14,25 +12,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/images/");
   // Copy `img/favicon/` to `_site/`
   eleventyConfig.addPassthroughCopy({ "./src/favicon": "/" });
-  // Copy image folder in posts to /blog
-  eleventyConfig.addPassthroughCopy({ "./src/posts/media": "/blog/media" });
-
-  // Returns programmes, sorted by display order
-  eleventyConfig.addCollection("programmes", (collection) => {
-    return sortByDisplayOrder(
-      collection.getFilteredByGlob("./src/coaching/*.md")
-    );
-  });
-
-  // Returns work, sorted by display order
-  eleventyConfig.addCollection("work", (collection) => {
-    return sortByDisplayOrder(collection.getFilteredByGlob("./src/work/*.md"));
-  });
-
-  // Returns a collection of blog posts in reverse date order
-  eleventyConfig.addCollection("blog", (collection) => {
-    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
-  });
 
   eleventyConfig.addWatchTarget("./src/sass/");
 
--- src/index.md
+++ src/index.md
@@ -1,21 +1,9 @@
 ---
 layout: layouts/home.html
-title: "Product team coach"
-headerIntroBlock:
-  image: "/images/scott-at-desk.png"
-  imageAlt: "An image of Scott sitting at his desk behind a laptop"
-  text: "Lead UX Designer and Agile Design Coach."
-  buttonText: "Get in touch"
-  buttonUrl: "#footer"
-scrollingTitle:
-  - "Collaborate."
-  - "Innovate."
-programmes:
-  empowerURL: "/coaching/empower"
-  discoveryURL: "/coaching/discovery"
-  innovateURL: "/coaching/innovate"
-coachingCTA:
-  text: "Wrestling with a tough product challenge and looking to improve the way your teams work?"
-  buttonText: "Book a free intro chat"
-  buttonUrl: "/coaching/"
+title: "Hello World"
+url: "./style/"
 ---
+
+# Hello world
+
+View <a href="{{ url }}">Style Guide</a>
